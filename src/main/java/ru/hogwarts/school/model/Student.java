0b16b6063eb6579a8bea3855a8983ed8c7c021cd
Nadezhda-Frimerman <tomns@mail.ru>
2025-09-10 22:03:13 +0500

    streams is ready

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -15,9 +15,6 @@ public class Student {
     @ManyToOne
     @JoinColumn(name="faculty_id")
     private Faculty faculty;
-    @OneToOne (mappedBy = "student", fetch = FetchType.LAZY)
-    private Avatar avatar;
-
 
     public Faculty getFaculty() {
         return faculty;
